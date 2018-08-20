4e6715c7ecbde1570524bcff1b220bc778ba2136
graeme-winter <graeme.winter@gmail.com>
2018-08-20 08:26:39 +0100

    Work in progress on templater

--- templater.py
+++ templater.py
@@ -42,14 +42,63 @@ def read_miniCBF_image(image):
     return [token.strip() for token in value.split()]
 
   for k in parameters:
-    parameters[k] = tidy_parameter_value(parameters[k])
+    if not k.startswith('X-'):
+      parameters[k] = tidy_parameter_value(parameters[k])
 
   return cbf_header, parameters, image_data
+
+def parameters_to_dc_info(parameters):
+  distance_mm = 1000.0 * float(parameters['Detector_distance'][0])
+  beam_x_pixel, beam_y_pixel = map(float, parameters['Beam_xy'][:2])
+  wavelength = float(parameters['Wavelength'][0])
+  expt = float(parameters['Exposure_time'][0])
+  expp = float(parameters['Exposure_period'][0])
+  omega = float(parameters['Omega'][0])
+  domega = float(parameters['Omega_increment'][0])
+  cuttoff = int(parameters['Count_cutoff'][0])
+  wide = int(parameters['X-Binary-Size-Fastest-Dimension'][0])
+  high = int(parameters['X-Binary-Size-Second-Dimension'][0])
+
+  # put the parameters which would be filled in by camserver in an appropriate
+  # dictionary
+
+  camserver = {'_wave_':wavelength,
+               '_expt_':expt,
+               '_expp_':expp,
+               '_omega_':omega,
+               '_domega_':domega,
+               '_timestamp_':parameters[timestamp],
+               '_wide_':wide,
+               '_high_':high}
+
+  # add more things if we know more things
+
+  if 'Chi' in parameters:
+    camserver['_chi_'] = float(parameters['Chi'][0])
+    camserver['_dchi_'] = float(parameters['Chi_increment'][0])
+
+  if 'Phi' in parameters:
+    camserver['_phi_'] = float(parameters['Phi'][0])
+    camserver['_dphi_'] = float(parameters['Phi_increment'][0])
+
+  if 'Kappa' in parameters:
+    camserver['_kappa_'] = float(parameters['Kappa'][0])
+
+  dc_info = {'detector':{'distance_mm':distance_mm,
+                         'beam_x_pixel':beam_x_pixel,
+                         'beam_y_pixel':beam_y_pixel},
+             'beam':{},
+             'camserver':camserver}
+
+  return dc_info
 
 if __name__ == '__main__':
   import sys
 
   cbf_header, parameters, image_data = read_miniCBF_image(sys.argv[1])
+  generator = template_generator_factory(sys.argv[1], example_dc_info)
+  print(generator())
+
 
   for key in sorted(parameters):
     print(key, parameters[key])
