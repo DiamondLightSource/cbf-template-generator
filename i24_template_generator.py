42f3284978885ff44415cc2fb35a1d7ba3c34154
graeme-winter <graeme.winter@gmail.com>
2018-10-16 09:18:24 +0100

    I24 has pin and plate mode, add an optional constructor argument; will need to add this to the parent classes

--- i24_template_generator.py
+++ i24_template_generator.py
@@ -3,16 +3,25 @@ from __future__ import division, print_function
 from dls_template_generator import dls_template_generator
 
 class i24_template_generator(dls_template_generator):
-  def __init__(self, beamline, data_collection_info):
+  def __init__(self, beamline, data_collection_info, mode='pin'):
 
-    # TODO decide how to select which goniometer is in use at creation time -
-    # depend on a flag for goniometer name or pass in actual axes?
+    assert mode in ('tray', 'pin')
 
-    common = {'goniometer':{'axes':{
-      'omega':{'axis':(0,1,0), 'depends_on':'.'}
+    if mode == 'pin':
+      common = {'goniometer':{'axes':{
+        'omega':{'axis':(0,1,0), 'depends_on':'.'}
       }}, 'detector':{'name':'DECTRIS PILATUS3 6M', 'axes':{
         'z':{'axis':(0,0,-1), 'depends_on':'.'}
       }, 'fast':(1,0,0), 'slow':(0,-1,0)}}
+    elif mode == 'tray':
+      common = {'goniometer':{'axes':{
+        'omega':{'axis':(-1,0,0), 'depends_on':'.'}
+      }}, 'detector':{'name':'DECTRIS PILATUS3 6M', 'axes':{
+        'z':{'axis':(0,0,-1), 'depends_on':'.'}
+      }, 'fast':(1,0,0), 'slow':(0,-1,0)}}
+    else:
+      # we cannot get to here anyway...
+      raise RuntimeError, 'unknown mode %s' % mode
 
     self.recursive_update(common, data_collection_info)
     dls_template_generator.__init__(self, beamline, common)
